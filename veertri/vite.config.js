e9cc1503f6c9230374ee94ca98ec83b46beb8864
shreejanmishra <71009867+shreejanmishra@users.noreply.github.com>
2026-01-11 04:13:19 +0530

    ROI metrics not populating
    The api call was routed to local host, hence the data was not getting fetched into vercel deployment.

--- veertri/vite.config.js
+++ veertri/vite.config.js
@@ -4,6 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   build: {
     rollupOptions: {
       output: {
